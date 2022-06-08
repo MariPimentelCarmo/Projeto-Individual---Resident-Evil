425bd8f3803d89b33eb13f2b183d8be5b3c0515e
MariPimentelCarmo <mariana.carmo@sptech.school>
2022-06-08 15:57:10 -0300

    Criação das ultimas páginas e botão de sair

--- public/JS/script.js
+++ public/JS/script.js
@@ -27,3 +27,24 @@ function abre_jogo() {
 function volta_inicio() {
     window.location.href = "inicio.html"
 }
+
+function abre_criaturas() {
+    window.location.href = "criaturas.html"
+}
+
+function abre_virus() {
+    window.location.href = "virus.html"
+}
+
+function abre_organizacoes() {
+    window.location.href = "organizacoes.html"
+}
+
+function abre_personagens() {
+    window.location.href = "personagens.html"
+}
+
+function sair() {
+    sessionStorage.clear;
+    window.location.href = "index.html"
+}
